c5f5679a054baf7312287003a4fd85b66d24e5a0
DewiGirot <dewi.girot@etu.iut-tlse3.fr>
2021-05-23 12:48:39 +0200

    Début code Cesar
    Ajout du main et lancement du code césar

--- main.c
+++ main.c
@@ -19,4 +19,40 @@
 *  Nom du fichier :  main.c                                                   *
 *                                                                             *
 ******************************************************************************/
+#include <stdio.h>
+#define ALPHABET 26
 
+void cesar(char texte[], int clef);
+
+
+void main(){
+    char texte[] = "DAIZ";
+    cesar(texte, 2);
+}
+	
+void cesar(char texte[], int clef) {
+
+	char tab[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i = 0;
+	clef = clef % 26;
+
+	while(texte[i] != '\0'){
+		int f = 0;
+		
+		printf("i avant %c\n", texte[i]);
+		while(tab[f] != texte[i]){
+			f++;
+		}
+		
+		//if ( i + clef > ALPHABET ) {
+			texte[i] = tab[(f + clef - ALPHABET)];
+		 } else { 
+		 	texte[i] = tab[(f + clef)];
+		 }
+			
+		
+		printf("Voici %c\n", texte[i]);
+		i++;
+	}	
+	
+}
