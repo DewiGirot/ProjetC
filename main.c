892bc23bb9e6a431e372c347b2c419ee30ab069e
DewiGirot <dewi.girot@etu.iut-tlse3.fr>
2021-05-28 23:00:53 +0200

    Upadate Déwi
    Ajout dans verificationCaracteresSpeciaux idée et ajout dans le main

--- main.c
+++ main.c
@@ -28,16 +28,17 @@ int verificationCaracteresSpeciaux (char texte []);
 
 void main () {
 
-         char texte[50];
+        char texte[50];
         int clef;
         int chiffrage;
 
         printf("Entrez votre message : \n");
         scanf("%s",&texte);
 
-
         printf("Entrez votre clef : \n");
         scanf("%d",&clef);
+        
+        
         while (verificationCode(clef) == -1) {
                 printf("Erreur dans la saisie de la clef ! \n\n");
                 printf("Entrez votre code et votre clef : \n");
@@ -51,6 +52,13 @@ void main () {
         while (verificationChiffrage(chiffrage) == -1) {
                 printf("Erreur veuillez reessayer : ");
                 scanf("%d",&chiffrage);
+        }
+        
+        if(verificationChiffrage(chiffrage) == 1){
+        	if (verificationCaracteresSpeciaux(texte) == 1) {
+                printf("Votre texte possède des caractères spéciaux, ils ne changeront donc pas");
+        	}  
+        	      
         }
 
 }
@@ -71,5 +79,14 @@ int verificationChiffrage (int chiffrage) {
 
 
 int verificationCaracteresSpeciaux (char texte [] ) {
-
+	//char caractereSpeciaux [] = {'&', 'À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ò', 'Ó', 'Ô', 'Õ', 'Ù', 'Ú', 'Û', 'à', 'á', 'â', 'ã', 'ä', 'å', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ô', 'õ', 'ö', 'ù', 'ú', 'û', 'ü', 'ý'};
+	
+	//for(int i = 0, i<sizeof(texte), i++){
+	//	int f = sizeof(caractereSpeciaux);
+	//	for(int x = 0, x<f, x++){
+	//		if(texte[i] == caractereSpeciaux[f]){
+	//			printf("Erreur, vous avez un ou plusieurs caractère spécial"); 
+	//		}
+	//	}
+	//}
 }
