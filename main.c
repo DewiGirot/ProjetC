be2e36a256f524f2727767832225dce54597ca8b
DewiGirot <dewi.girot@etu.iut-tlse3.fr>
2021-06-16 16:03:29 +0200

    update majuscules
    Ajouts de la possibilité de majuscules dans le texte, rectification et réécritures des textes d'erreurs et rectification de verificationCode

--- main.c
+++ main.c
@@ -25,6 +25,7 @@
 #include <stdlib.h>
 
 int verificationCode (int clef);
+int verificationLongueurTexte(char texte[]);
 int verificationChiffrage (int chiffrage);
 int verificationCaracteresSpeciaux (char texte []);
 void cesar (char texte[], int clef, int chiffrage);
@@ -38,24 +39,30 @@ void main () {
 
         printf("Entrez votre message, attention il ne doit pas contenir des caractères spéciaux et ne pas doit dépasser les 100 caractères : \n     ");
         scanf("%s",&texte);
-        while (verificationCaracteresSpeciaux(texte) == 1) {
-        	printf("Erreur veuillez réessayer : \n     ");
+        
+        while (verificationLongueurTexte(texte) == -1) {
+        	printf("Erreur veuillez donnez un texte inférieur à 100 caractères : \n     ");
         	scanf("%s",&texte);
         	printf("\n",texte);
 	}
         
+        while (verificationCaracteresSpeciaux(texte) == 1) {
+        	printf("Erreur veuillez entrer un texte sans caractères spéciaux : \n     ");
+        	scanf("%s",&texte);
+        	printf("\n",texte);
+	}
 
-        printf("Entrez votre clef : \n     ");
+        printf("Entrez votre clef comprise entre 0 et 26 non compris : \n     ");
         scanf("%d",&clef);
         while ( verificationCode(clef) == -1 ) {
 		printf("Erreur veuillez réessayer : \n     ");
 		scanf("%d",&clef);
 	}
 	
-        printf("Voulez vous chifrer ou déchiffrer le message ? ( 1 pour chiffrer et 0 pour déchiffrer)\n     ");
+        printf("Voulez vous chifrer ou déchiffrer le message ? (1 pour chiffrer et 0 pour déchiffrer)\n     ");
         scanf("%d",&chiffrage);
         while ( verificationChiffrage(chiffrage) == -1 ) {
-		printf("Erreur veuillez réessayer : \n     ");
+		printf("Erreur veuillez saisir 1 pour chiffrer et 0 pour déchiffrer : \n     ");
 		scanf("%d",&chiffrage);
 	}
         
@@ -66,9 +73,15 @@ void main () {
 
 }
 
+int verificationLongueurTexte(char texte[]){
+	if (strlen(texte) >= 100) {
+                return -1 ;
+        }
+        return 1;
+}
 
 int verificationCode (int clef) {
-        if (clef>26 || clef<1  ) {
+        if (clef>25 || clef<1  ) {
                 return -1 ;
         }
         return 1;
@@ -81,19 +94,32 @@ int verificationChiffrage(int chiffrage) {
 	return -1;
 }
 
-
 void cesar (char texte[],int clef,int chiffrage) {
-    for(int i=0 ; i<strlen(texte) ; i++)
-    	if ( chiffrage == 1 )
-            texte[i] = 'a' + ((texte[i] - 'a') + clef)%26;
-        else {
-        	if(('a' + ((texte[i] - 'a') - clef)) < 'a')
-        		texte[i] = 'z' + 1 + ((texte[i] - 'a') - clef);
-        	else
-        		texte[i] = 'a' + ((texte[i] - 'a') - clef);
-        	
-        }   	
-    
+	for(int i=0 ; i<strlen(texte) ; i++){
+	    	if ( chiffrage == 1 ){
+			if ('a' <= texte[i] && texte[i] <= 'z')
+		    		texte[i] = 'a' + ((texte[i] - 'a') + clef)%26;
+			else if ('A' <= texte[i] && texte[i] <= 'Z')
+				texte[i] = 'A' + ((texte[i] - 'A') + clef)%26;
+			/*else
+		        	if ('0' <= texte[i] && texte[i] <= '9')
+		            		texte[i] = '0' + ((texte[i] - '0') + clef)%10;	*/  	
+	    
+		else
+			if ('a' <= texte[i] && texte[i] <= 'z'){
+				if(('a' + ((texte[i] - 'a') - clef)) < 'a')
+					texte[i] = 'z' + 1 + ((texte[i] - 'a') - clef);
+				else
+					texte[i] = 'a' + ((texte[i] - 'a') - clef);
+			}else if ('A' <= texte[i] && texte[i] <= 'Z'){
+				if(('A' + ((texte[i] - 'Z') - clef)) < 'A')
+					texte[i] = 'Z' + 1 + ((texte[i] - 'A') - clef);
+				else
+					texte[i] = 'A' + ((texte[i] - 'A') - clef);
+			}
+		}
+	}
+       
 }
 
 int verificationCaracteresSpeciaux (char texteATester[]) { 
